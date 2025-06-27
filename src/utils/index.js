0cb1b5101df8c723774e7885e41f755d9041fcac
CatalinaVilches <catalina.vilchesf99@gmail.com>
2025-06-27 11:50:54 -0400

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,18 +2,15 @@ import bcrypt from 'bcrypt';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-// Función para generar hash de una contraseña
 export const createHash = async (password) => {
   const salt = await bcrypt.genSalt(10);
   return bcrypt.hash(password, salt);
 };
 
-// Función para validar una contraseña con su hash
 export const passwordValidation = async (user, password) => {
   return bcrypt.compare(password, user.password);
 };
 
-// Obtener __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
