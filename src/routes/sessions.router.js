258364258acc96162dd299d70e272bbd78748e2e
CatalinaVilches <catalina.vilchesf99@gmail.com>
2025-06-27 11:51:35 -0400

    Update sessions.router.js

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -3,11 +3,9 @@ import sessionsController from '../controllers/sessions.controller.js';
 
 const router = Router();
 
-// Endpoints de autenticación
 router.post('/register', sessionsController.register);
 router.post('/login', sessionsController.login);
 
-// Endpoints protegidos y no protegidos
 router.get('/current', sessionsController.current);
 router.get('/unprotectedLogin', sessionsController.unprotectedLogin);
 router.get('/unprotectedCurrent', sessionsController.unprotectedCurrent);
